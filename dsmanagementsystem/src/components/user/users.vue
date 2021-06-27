f0eb67450cf0c4008ad9f12d6d56369ee3d3e27c
lijiabin0930 <987936361@qq.com>
2021-06-28 00:15:18 +0800

    添加用户表单

--- dsmanagementsystem/src/components/user/users.vue
+++ dsmanagementsystem/src/components/user/users.vue
@@ -13,11 +13,11 @@
       <!--      span 一行总共24span-->
       <el-row :gutter="20">
         <el-col :span="7">
-          <el-input placeholder="请输入内容">
-          <el-button slot="append" icon="el-icon-search"></el-button>
+          <el-input v-model="queryinfo.query" clearable placeholder="请输入内容" @clear="getUserList">
+          <el-button slot="append" icon="el-icon-search" @click="getUserList"></el-button>
           </el-input>
         </el-col>
-        <el-col :span="4"><el-button type="primary">添加用户</el-button></el-col>
+        <el-col :span="4"><el-button type="primary" @click="changeAddDialogVisible">添加用户</el-button></el-col>
       </el-row>
       </div>
     <!--     用户列表区域 显示table内容-->
@@ -27,26 +27,26 @@
           style="width: 100%">
     <!--        自定义索引列-->
         <el-table-column
+            :index="indexMethod"
             label="id"
-            type="index"
-            :index="indexMethod">
+            type="index">
         </el-table-column>
         <el-table-column
-            prop="username"
             label="姓名"
+            prop="username"
             width="180">
         </el-table-column>
         <el-table-column
-            prop="email"
-            label="邮箱">
+            label="邮箱"
+            prop="email">
         </el-table-column>
         <el-table-column
-            prop="mobile"
-            label="电话">
+            label="电话"
+            prop="mobile">
         </el-table-column>
         <el-table-column
-            prop="role_name"
-            label="角色">
+            label="角色"
+            prop="role_name">
         </el-table-column>
         <el-table-column
             label="状态">
@@ -60,17 +60,17 @@
             label="操作">
 <!--          slot-scope="scope" 可以接收从子组建传出来的结果 通过scope.row.xx可以拿到数据结果 row是当时子组建内部:row绑定的-->
           <template slot-scope="scope">
-            <el-tooltip  effect="dark" content="修改用户" placement="top-start" :enterable="false">
+            <el-tooltip  :enterable="false" content="修改用户" effect="dark" placement="top-start">
               <!--            修改按钮-->
-              <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+              <el-button icon="el-icon-edit" size="mini" type="primary"></el-button>
             </el-tooltip>
-            <el-tooltip  effect="dark" content="删除用户" placement="top-start" :enterable="false">
+            <el-tooltip  :enterable="false" content="删除用户" effect="dark" placement="top-start">
             <!--            删除按钮-->
-            <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+            <el-button icon="el-icon-delete" size="mini" type="danger"></el-button>
             </el-tooltip>
-            <el-tooltip  effect="dark" content="分配角色" placement="top-start" :enterable="false">
+            <el-tooltip  :enterable="false" content="分配角色" effect="dark" placement="top-start">
           <!--            分配角色按钮-->
-            <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
+            <el-button icon="el-icon-setting" size="mini" type="warning"></el-button>
             </el-tooltip>
           </template>
         </el-table-column>
@@ -78,15 +78,46 @@
 
       <!--      分页区域-->
       <el-pagination
-          @size-change="handleSizeChange"
-          @current-change="handleCurrentChange"
           :current-page="queryinfo.pagenum"
-          :page-sizes="[1,2,5,10]"
           :page-size="queryinfo.pagesize"
+          :page-sizes="[1,2,5,10]"
+          :total="total"
           layout="total, sizes, prev, pager, next, jumper"
-          :total="total">
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange">
       </el-pagination>
     </el-card>
+
+    //额外的对话框 点击后弹出的
+    <el-dialog
+        :visible.sync="addDialogVisible"
+        title="添加用户"
+        width="50%">
+    <!--      内容主体区-->
+      <el-form ref="addFormRef"  :model="addForm" :rules="addFormRules" label-width="70px">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="addForm.username"  placeholder="请输入用户名" ></el-input>
+        </el-form-item>
+
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="addForm.password"  placeholder="请输入密码" ></el-input>
+        </el-form-item>
+
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="addForm.email"  placeholder="请输入邮箱" ></el-input>
+        </el-form-item>
+
+        <el-form-item label="手机" prop="mobile">
+          <el-input v-model="addForm.mobile"  placeholder="请输入手机号" ></el-input>
+        </el-form-item>
+
+      </el-form>
+    <!--      底部区域-->
+      <span slot="footer" class="dialog-footer">
+    <el-button @click="addDialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="addDialogVisible = false">确 定</el-button>
+  </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -105,6 +136,32 @@
         queryinfo: {query: '', pagenum: 1, pagesize: 2} ,
         userlist : [],
         total: 0 ,
+        addDialogVisible: false, //控制对话框的显示与隐藏,
+        //添加用户的内容
+        addForm:{
+          username: "",
+          password: "",
+          email: "",
+          mobile: ""
+        } ,
+        //添加增加用户的验证规则
+        addFormRules: {
+          username : [
+            { required: true, message: '请输入用户名', trigger: 'blur' },
+            { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+            ],
+          password:[
+            { required: true, message: '请输入密码', trigger: 'blur' },
+            { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+          ],
+          email: [
+            { required: true, message: '请输入邮箱', trigger: 'blur' },
+          ],
+          mobile: [
+            { required: true, message: '请输入手机号', trigger: 'blur' },
+          ]
+
+        }
       }
     },
      methods: {
@@ -141,7 +198,16 @@
          const {data :res }= await this.$http.put(`users/${id}/state/${state}`)
          this.print(res)
          //如果更改成功
-       }
+         if (res.meta.status !== 200){
+           userinfo.mg_state = !userinfo.mg_state
+           return this.$message.error("更新用户状态失败")
+         }
+         //刷新页面一次
+         return this.$message.success("更新用户状态成功！")
+       },
+       changeAddDialogVisible(){
+        this.addDialogVisible = true
+       },
     }
 }
 </script>
