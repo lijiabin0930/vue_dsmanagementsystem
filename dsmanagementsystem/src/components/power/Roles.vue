99ae62e942bdcea409deaa2cb55698da3eb892e1
lijiabin0930 <987936361@qq.com>
2021-06-29 23:14:46 +0800

    权限管理

--- dsmanagementsystem/src/components/power/Roles.vue
+++ dsmanagementsystem/src/components/power/Roles.vue
@@ -15,8 +15,52 @@
         </el-col>
       </el-row>
       <!--      角色列表区域-->
+      <el-table
+          :data="roleList" border stripe>
+        <!--        扩展列-->
+        <el-table-column type="expand">
+          <template slot-scope="scope">
+<!--            栅格系统的分配思路是： row是一行 一行里 共有24个span 16+8=24-->
+            <el-row>
+    <!--              一级权限里 占了5个格子-->
+              <el-col :span="5">
+              <!--               渲染一级权限   -->
+                <el-button>lallalala</el-button>
+              </el-col>
 
+    <!--              二级三级占19个格子 一行-->
+              <el-col :span="19">
+              <!--                  渲染二级和三级权限-->
+                <el-button>lallalala</el-button>
+                <el-button>lallalala</el-button>
+              </el-col>
+            </el-row>
+            <pre>
+              {{scope.row}}
+            </pre>
+          </template>
+        </el-table-column>
+        <!--        索引列-->
+        <el-table-column type="index">
+        </el-table-column>
+        <el-table-column
+            prop="roleName"
+            label="角色名称" align="center">
+        </el-table-column>
+        <el-table-column
+            prop="roleDesc"
+            label="角色描述" align="center">
+        </el-table-column>
+        <el-table-column
+            label="操作" align="center" width="300px">
+          <template slot-scope="scope">
+            <el-button size="mini" type="primary" icon="el-icon-edit">编辑</el-button>
+            <el-button size="mini" type="danger" icon="el-icon-delete">删除</el-button>
+            <el-button size="mini" type="warning" icon="el-icon-setting">分配权限</el-button>
+          </template>
+        </el-table-column>
 
+      </el-table>
     </el-card>
   </div>
 </template>
