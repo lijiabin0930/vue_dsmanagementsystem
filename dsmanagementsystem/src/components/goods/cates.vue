1f7a9d7a97cccaf49116cfb1d5aaca25f952dd7c
lijiabin0930 <987936361@qq.com>
2021-07-16 23:40:40 +0800

    new

--- dsmanagementsystem/src/components/goods/cates.vue
+++ dsmanagementsystem/src/components/goods/cates.vue
@@ -18,7 +18,6 @@
                   :data="goodsList" :expand-type="false" :selection-type='false' :show-row-hover="false"
                   border index-text="#" show-index>
         <template slot="isOk" slot-scope="scope">
-
           <i v-if="scope.row.cat_deleted===false" class="el-icon-success"></i>
           <i v-else class="el-icon-error"></i>
         </template>
@@ -30,8 +29,8 @@
         </template>
         <!--        操作-->
         <template slot="operate" slot-scope="scope">
-          <el-button size="mini" type="primary">编辑</el-button>
-          <el-button size="mini" type="danger">删除</el-button>
+          <el-button size="mini" type="primary" @click="openEditorDialog(scope.row)">编辑</el-button>
+          <el-button size="mini" type="danger" @click="deleteEditorCat(scope.row)">删除</el-button>
         </template>
 
       </tree-table>
@@ -71,6 +70,24 @@
     <el-button type="primary" @click="addCate('ruleForm')">确 定</el-button>
   </span>
     </el-dialog>
+
+    <!--    编辑分类名称-->
+    <el-dialog
+        :visible.sync="eidtorDialogVisible"
+        title="编辑分类"
+        width="50%">
+      <!--          添加分类的表单-->
+      <el-form ref="ruleForm" :model="ruleEditeForm" :rules="rules" label-width="100px">
+        <el-form-item label="分类名称" prop="cat_name">
+          <el-input v-model="ruleEditeForm.cat_name" placeholder="请编辑分类名称"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+    <el-button @click="closeCate">取 消</el-button>
+    <el-button type="primary" @click="editeCate('ruleForm')">确 定</el-button>
+  </span>
+    </el-dialog>
+
   </div>
 </template>
 
@@ -98,12 +115,19 @@ export default {
         cat_name: '',
         cat_pid: 0, cat_level: 0
       },
+      //编辑分类名称,
+      ruleEditeForm: {
+        cat_name: ''
+      },
       //表单验证
       rules: {
         cat_name: [{required: true, message: '请输入分类名称', trigger: 'blur'}],
       },
       catLists: [],
-      selectKeys: [] ,//选中的数组放数据的地方 必须是数组
+      selectKeys: [],//选中的数组放数据的地方 必须是数组
+      eidtorDialogVisible: false,
+      // 获取点击该行 获得该商品的cat_id 用于更新该商品的名称
+      idEditCat: "",
     }
   },
   methods: {
@@ -134,27 +158,27 @@ export default {
       this.addStoreDialogVisible = true
       //显示窗口的同时 在把数据请求出来 用于后续展示输入框使用
       //请求商品分类列表
-      const {data :res} = await this.$http.get('categories',{params:{type:2}})
-      if (res.meta.status !== 200){
+      const {data: res} = await this.$http.get('categories', {params: {type: 2}})
+      if (res.meta.status !== 200) {
         return this.$message.error('请求商品分类列表失败！')
       }
       this.catLists = res.data  //将请求的一级二级分类拿到后绑定给 this.catList
     },
     //选择项发生变化就触发
-    parentsHandleChange(){
+    parentsHandleChange() {
       console.log(this.selectKeys)
       //长度大于0 选中了父级分类
-      if (this.selectKeys.length > 0){
-        this.ruleForm.cat_pid = this.selectKeys[this.selectKeys.length-1]
+      if (this.selectKeys.length > 0) {
+        this.ruleForm.cat_pid = this.selectKeys[this.selectKeys.length - 1]
         this.ruleForm.cat_level = this.selectKeys.length
         return
-      }else {
+      } else {
         this.ruleForm.cat_pid = 0
         this.ruleForm.cat_level = 0
       }
     },
     //关闭对话框时候 要清空ruleFrom下次进来是空的
-    closeCate(){
+    closeCate() {
       //清空表单
       this.$refs.ruleForm.resetFields()
       this.selectKeys = []
@@ -165,30 +189,68 @@ export default {
       this.addStoreDialogVisible = false
     },
     //确定添加分类
-    addCate(refName){
+    addCate(refName) {
       this.$refs[refName].validate().then(async validate => {
-        if (validate) {
-        const {data :res}  = await this.$http.post(
-            'categories',this.ruleForm
+            if (validate) {
+              const {data: res} = await this.$http.post(
+                  'categories', this.ruleForm
+              )
+              if (res.meta.status !== 201) {
+                return this.$message.error('添加商品分类失败!')
 
-          )
-          if (res.meta.status !== 201){
-            return this.$message.error('添加商品分类失败!')
-
-          }
-        return  this.$message.success("添加商品分类成功！")
-        }
+              }
+              this.closeCate()
+              return this.$message.success("添加商品分类成功！")
+            }
           }
       ).catch(error => {
-            console.log(error);
-            return this.$message(
-                {
-                  message: '添加分类失败，验证分类名称失败了！',
-                  type: 'error',
-                  showClose: true
-                }
-            )
-          })
+        console.log(error);
+        return this.$message(
+            {
+              message: '添加分类失败，验证分类名称失败了！',
+              type: 'error',
+              showClose: true
+            }
+        )
+      })
+    },
+    //编辑商品分类名字插槽
+    openEditorDialog(data) {
+      // console.log(data,">>>>")
+      //拿到id cat_name -> 新修改的值
+      this.ruleEditeForm.cat_name = data.cat_name
+      this.idEditCat = data.cat_id
+      this.eidtorDialogVisible = true
+    },
+    //将编辑好的新的商品名字更新到后台服务器中
+    async editeCate(ruleForm) {
+      //  请求后台更新逻辑
+      const {data: res} = await this.$http.put(`categories/${this.idEditCat}`,
+          {cat_name: this.ruleEditeForm.cat_name})
+      if (res.meta.status !== 200) {
+        this.idEditCat = ''
+        this.ruleEditeForm.cat_name = ''
+        return this.$message.error('编辑失败！！！')
+      }
+      // idEditCat 滞空
+      this.idEditCat = ''
+      this.ruleEditeForm.cat_name = ''
+      await this.getCategories()
+      this.eidtorDialogVisible = false
+      return this.$message.success('编辑成功！')
+    },
+    //删除分类
+    async deleteEditorCat(data) {
+      this.idEditCat = data.cat_id
+      const {data: res} = await this.$http.delete(`categories/${this.idEditCat}`)
+      if (res.meta.status !== 200) {
+        this.idEditCat = ''
+        return this.$message.error('删除失败！！！')
+      }
+      this.idEditCat = ''
+      await this.getCategories()
+      return this.$message.success('删除成功！')
+
     }
   }
 }
@@ -225,6 +287,7 @@ export default {
 .zk-table {
   margin-top: 10px;
 }
+
 .el-cascader {
   width: 100%;
 }
